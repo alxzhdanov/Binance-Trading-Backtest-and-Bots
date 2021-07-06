793fad8a983bf7ce33fb6c94079a3eab8fac3707
c-s-westphal <67380062+c-s-westphal@users.noreply.github.com>
2021-07-06 12:45:26 +0100

    Update linreg_5min.py

--- Lin_reg_trader/linreg_5min.py
+++ Lin_reg_trader/linreg_5min.py
@@ -25,7 +25,7 @@ from sklearn.linear_model import LinearRegression
 import schedule
 import time
 
-client = Client('EddVzZblDuedZlr1E2UCMKx83Uf5djw9LPtzoIqJDHgrIngRE6AW5irkRQYTLxvi', '1f5OGwgXzY4tGx9SJMORzKXsK60hksFI60d9LWTyLtj03Xm2iljKawNAopDvuUjK',{"verify": False, "timeout": 200000})
+client = Client('key', 'secret key',{"verify": False, "timeout": 200000})
 
 def symbols():
 
